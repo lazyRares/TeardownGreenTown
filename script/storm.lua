89306d618357c32728cc9a3681f50dc151741b5b
onethousandvolts <138905188+onethousandvolts@users.noreply.github.com>
2024-11-09 03:23:28 -0700

    fixed oopsie with propag
    also added storm sfx

---
+++ script/storm.lua
@@ -0,0 +1,24 @@
+function init()
+    form = LoadSound("MOD/snd/storm_forming.ogg")
+    advance = LoadSound("MOD/snd/storm_advance.ogg")
+
+    timer = 0
+    next_advance = 60
+    formed = false
+end
+
+function tick(dt)
+    timer = timer + dt
+
+    if timer > 7 and not formed then
+        timer = 0
+        formed = true
+        PlaySound(form, GetPlayerTransform().pos, 0.2)
+    end
+
+    if timer > next_advance then
+        timer = 0
+        PlaySound(advance)
+        next_advance = math.random(75, 180)
+    end
+end
